1ca96fc69201d6127e23baf4279f30998f57289e
rnfrafael <rnf.rafael@gmail.com>
2023-04-28 10:20:15 -0300

    add: routes, contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { NavBar } from "./components";
+import { Login, MainPage } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { userNameContext } from "./contexts/userNameContext";
+import { postContext } from "./contexts/postContext";
+import { IPosts } from "./repositories/postRepository";
+import { IUsers, userRemove, userSave } from "./repositories/usersRepository";
+import { postSave, postRemove } from "./repositories/postRepository";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const [user, setUserName] = useState<IUsers | undefined>();
+  const [posts, setPosts] = useState<IPosts[]>([]);
 
   return (
-    
-  )
+    <userNameContext.Provider
+      value={{ user, setUserName, userSave, userRemove }}
+    >
+      <postContext.Provider value={{ posts, setPosts, postSave, postRemove }}>
+        <div className="flex flex-col w-full h-screen items-center">
+          <BrowserRouter>
+            <NavBar />
+            <Routes>
+              <Route index element={<Login />} />
+              <Route path="/" element={<Login />}></Route>
+              <Route path="/mainpage" element={<MainPage />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </postContext.Provider>
+    </userNameContext.Provider>
+  );
 }
-
-export default App
